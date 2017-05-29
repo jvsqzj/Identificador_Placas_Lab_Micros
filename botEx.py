f3a17c8a7a265ae8be9ff24cbc6bd9c3588cce66
Camila Gómez <camilagm994@gmail.com>
2017-05-29 17:52:15 -0600

    lala

--- botEx.py
+++ botEx.py
@@ -6,7 +6,10 @@ import logging
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Bienvenido, nuevo usuario. \nEste bot se encarga de notificar y procesar los cobros de peaje y parqueo del sistema automatizado.")
     bot.send_message(chat_id=update.message.chat_id, text="Para iniciar, ingrese su # de placa para que pueda ser ingresado en el sistema.")
-    placa = getPlaca(bot, update)
+    states={
+    placa: [MessageHandler(Filters.text, getPlaca)]
+    },
+    #print(placa)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,7 +25,8 @@ updater.start_polling()
 def getPlaca(bot, update):
     user = update.message.from_user
     logger.info("License of %s: %s" % (user.first_name, update.message.text))
-    placa = int(message.text)
+    placa = ("%s" % update.message.text)
+    print(placa)
     update.message.reply_text('Todo listo. Ahora recibiras una notificacion cada vez que haya un cobro pendiente.',
                               reply_markup=ReplyKeyboardRemove())
 
