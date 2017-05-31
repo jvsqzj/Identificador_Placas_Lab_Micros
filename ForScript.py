a74f92b824a49f37ecbd184a58a19fecd0ca1328
Leon-SA <sleon08@hotmail.es>
2017-05-31 12:31:49 -0600

    GUI actualizada.

--- ForScript.py
+++ ForScript.py
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*- # Para caracteres especiales.
 
 import os # Acceso a terminal.
+import random
 from Tkinter import * # Para interfaz grafica.
+
+def show(window):
+    window.deiconify()
+
+def hide(window):
+    window.withdraw()
 
 def execute(f):
     W0.after(200,f)
@@ -11,12 +18,43 @@ def printf(text):
     print text # Imprime un texto
 
 def exe_script():
-    os.system("./Script.sh")
+    #os.system("./Script.sh")
+    print "Script."
+
+def filetoll():
+    f = open('archivo.txt', 'w')
+    f.write('peaje\n0')
+    f.close()
+
+def fileparking():
+    f2 = open('archivo.txt', 'w')
+    a = random.randrange(11)
+    b = str(a)
+    f2.write('parqueo\n'+b)
+    f2.close()
 
 W0 = Tk() # Abre ventana.
 
-B1 = Button(W0, text="Tomar foto y procesar", command=lambda: exe_script()).pack()
+W1 = Toplevel(W0) # Ventana hija.
+W2 = Toplevel(W0) # Ventana hija.
+W1.protocol("WM_DELETE_WINDOW", "onexit") # Elimina la opción de salir para evitar el error.
+W2.protocol("WM_DELETE_WINDOW", "onexit") # Elimina la opción de salir para evitar el error.
 
+B0 = Button(W0, text="Sistema Peaje", command=lambda: execute(show(W1))).pack()
+B1 = Button(W0, text="Sistema Parqueo", command=lambda: execute(show(W2))).pack()
 B2 = Button(W0, text="Salir", command=lambda: W0.destroy()).pack()
 
+B3 = Button(W1, text="Fotografiar", command=lambda: exe_script() or filetoll()).pack()
+B4 = Button(W1, text="Cancelar", command=lambda: execute(hide(W1))).pack()
+
+B5 = Button(W2, text="Fotografiar", command=lambda: exe_script() or fileparking()).pack()
+B6 = Button(W2, text="Cancelar", command=lambda: execute(hide(W2))).pack()
+
+#B1 = Button(W0, text="Tomar foto y procesarla", command=lambda: execute(show(W1))).pack()
+#B3 = Button(W1, text="Sistema Peaje", command=lambda: exe_script() or filetoll()).pack()
+#B4 = Button(W1, text="Sistema Parqueo", command=lambda: exe_script() or fileparking()).pack()
+#B5 = Button(W1, text="Cancelar", command=lambda: execute(hide(W1))).pack()
+
+W1.withdraw() # Oculta ventana.
+W2.withdraw() # Oculta ventana.
 W0.mainloop() # Bucle principal.
