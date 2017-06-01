834343576387ea13d004c18c14fc7cc1a05f693b
jvsqzj <jvsqzj@gmail.com>
2017-06-01 00:29:59 -0600

    Agregando cálculo de cobro y envío de mensaje

--- Receive.py
+++ Receive.py
@@ -1,4 +1,4 @@
-import socket
+import socket, dataBase
 
 UDP_IP = "169.254.135.223" #Direccion IP Destino
 UDP_PORT = 5005
@@ -10,3 +10,10 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print ("MENSAJE:"), data
+    if data != None:
+        dataS = data.split()
+        modo = dataS[0]
+        horas = dataS[1]
+        placa = dataS[2]
+
+        dataBase.SEND(129323720,data)
