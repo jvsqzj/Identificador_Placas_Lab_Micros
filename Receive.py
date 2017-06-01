2dac4f62b97769b75ce3054cf52647baadf3add7
jvsqzj <jvsqzj@gmail.com>
2017-06-01 00:41:11 -0600

    Mensajes de notificación agregados

--- Receive.py
+++ Receive.py
@@ -1,6 +1,6 @@
 import socket, dataBase
 
-UDP_IP = "169.254.135.223" # Direccion IP Destino
+UDP_IP = "169.254.135.223" #Direccion IP Destino
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -15,5 +15,12 @@ while True:
         modo = dataS[0]
         horas = dataS[1]
         placa = dataS[2]
-
-        dataBase.SEND(129323720,data)
+        chatID = dataBase.buscarID(placa)
+        mensaje = ''
+        precio = 150
+        precioHoras = 850*int(horas)
+        if modo == 'peaje':
+            mensaje = ('Usted tiene un cobro de peaje pendiente por %d colones, al vehículo con placa %s.' %(precio, placa))
+        else if modo == 'parqueo':
+            mensaje = ('El monto a pagar por %s de parqueo es de %d colones, al vehículo con placa %s.' %(horas, precioHoras, placa))
+        dataBase.SEND(chatID,mensaje)
